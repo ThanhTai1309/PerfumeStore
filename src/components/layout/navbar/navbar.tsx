75165ed4fc4659af45fbb1cf7c23952dec21dce0
Finn MAC <macbook@MacBookPro.lan>
2025-03-06 16:57:42 +0700

    design nav bar

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,18 +1,58 @@
 import { JSX } from 'react';
 import { Link } from "react-router-dom";
 import { AppBar, IconButton, Typography, Stack, Toolbar, Box } from "@mui/material";
-import ShopIcon from '@mui/icons-material/Shop';
 import SearchIcon from '@mui/icons-material/Search';
 import PhoneIcon from '@mui/icons-material/Phone';
 import styles from '../navbar/style/navbar.module.css';
 import { direction } from './direction';
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
 
 const Navbar = (): JSX.Element => {
   return (
     <AppBar
       position="absolute"
       sx={{
-        backgroundColor: "rgba(0, 51, 102, 0.9)", // Xanh đậm với độ mờ
+        backgroundColor: "rgba(0, 102, 66, 0.9)", // Xanh đậm với độ mờ
         backdropFilter: "blur(8px)",
         width: "100%",
         top: 0,
@@ -30,21 +70,31 @@ const Navbar = (): JSX.Element => {
         {/* Menu */}
         <Stack direction="row" spacing={3} className={styles.directionStyle}>
           {direction.map((dir, index) => (
-            <Link key={index} to={dir.href} style={{ color: "white", textDecoration: "none", fontWeight: "bold" }}>
+            <Link key={index} to={dir.href} style={{ color: "white", textDecoration: "none", fontWeight: "bold", textAlign: 'center' }}>
               {dir.title}
             </Link>
           ))}
         </Stack>
 
         {/* Icons */}
-        <Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          {/* Ô tìm kiếm */}
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Tìm kiếm..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+
+          {/* Icon Giỏ Hàng */}
           <IconButton color="inherit">
-            <SearchIcon />
-          </IconButton>
-          <IconButton color="inherit">
-            <ShopIcon />
+            <ShoppingCartIcon />
           </IconButton>
         </Box>
+
       </Toolbar>
     </AppBar>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,35 +2,38 @@ import { Typography, Box } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <Box
-      sx={{
-        position: "relative",
-        width: "100%",
-        height: "600px",
-        backgroundImage: "url('/nuochoa.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        WebkitMaskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.3))",
-        maskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
-        mt: 12,
-      }}
-    >
-      {/* Slogan */}
-      <Typography
+    <>
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "600px",
+          backgroundImage: "url('/nuochoa.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          WebkitMaskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.3))",
+          maskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+          mt: 10,
+        }}
+      >
+      </Box>
+      {/* <Typography
         variant="h3"
         sx={{
           position: "absolute",
           top: "40%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          color: "white",
+          color: "black",
           fontWeight: "bold",
           textShadow: "2px 2px 8px rgba(0, 0, 0, 0.5)",
           fontFamily: "serif",
         }}
       >
-      </Typography>
-    </Box>
+        Danh Mục Sản Phẩm
+      </Typography> */}
+
+    </>
   );
 };
 
