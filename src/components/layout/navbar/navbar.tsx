28783e7dabed6ac13e9a4cc85cf61cd9e8ad532f
Finn MAC <macbook@MacBookPro.lan>
2025-03-06 11:51:14 +0700

    Homepage Demo

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,44 +1,53 @@
 import { JSX } from 'react';
-import ShopIcon from '@mui/icons-material/Shop';
 import { Link } from "react-router-dom";
-
-import {
-  AppBar,
-  IconButton,
-  Typography,
-  Stack,
-  Toolbar,
-  Box,
-} from "@mui/material"
-import { SHOP_NAME } from './constants';
-import styles from '../navbar/style/navbar.module.css'
+import { AppBar, IconButton, Typography, Stack, Toolbar, Box } from "@mui/material";
+import ShopIcon from '@mui/icons-material/Shop';
+import SearchIcon from '@mui/icons-material/Search';
+import PhoneIcon from '@mui/icons-material/Phone';
+import styles from '../navbar/style/navbar.module.css';
 import { direction } from './direction';
 
 const Navbar = (): JSX.Element => {
   return (
     <AppBar
+      position="absolute"
       sx={{
-        backgroundColor: 'green'
+        backgroundColor: "rgba(0, 51, 102, 0.9)", // Xanh đậm với độ mờ
+        backdropFilter: "blur(8px)",
+        width: "100%",
+        top: 0,
+        zIndex: 10,
+        padding: "10px 0",
       }}
     >
-      <Toolbar className={styles.toolBar}>
-        <Typography variant='h6' component='div'>
-          {SHOP_NAME}
-        </Typography>
-        <IconButton size='large' edge='start' color='inherit' arial-label='logo'>
-          <ShopIcon />
-        </IconButton>
+      <Toolbar className={styles.toolBar} sx={{ display: "flex", justifyContent: "space-between" }}>
+        {/* Hotline */}
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <PhoneIcon sx={{ marginRight: 1 }} />
+          <Typography variant="body1">HOTLINE: 0899489929</Typography>
+        </Box>
 
-        <Stack direction="row" spacing={2} className={styles.directionStyle}>
+        {/* Menu */}
+        <Stack direction="row" spacing={3} className={styles.directionStyle}>
           {direction.map((dir, index) => (
-            <Link key={index} to={dir.href} style={{ color: "inherit", textDecoration: "none" }}>
+            <Link key={index} to={dir.href} style={{ color: "white", textDecoration: "none", fontWeight: "bold" }}>
               {dir.title}
             </Link>
           ))}
         </Stack>
+
+        {/* Icons */}
+        <Box>
+          <IconButton color="inherit">
+            <SearchIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <ShopIcon />
+          </IconButton>
+        </Box>
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,37 @@
+import { Typography, Box } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <div>
-      Home Page
-    </div>
-  )
-}
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        height: "600px",
+        backgroundImage: "url('/nuochoa.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        WebkitMaskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.3))",
+        maskImage: "linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4))",
+        mt: 12,
+      }}
+    >
+      {/* Slogan */}
+      <Typography
+        variant="h3"
+        sx={{
+          position: "absolute",
+          top: "40%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          color: "white",
+          fontWeight: "bold",
+          textShadow: "2px 2px 8px rgba(0, 0, 0, 0.5)",
+          fontFamily: "serif",
+        }}
+      >
+      </Typography>
+    </Box>
+  );
+};
 
-export default HomePage
+export default HomePage;
